292ce259a3a19a25fd1d0261de81b483aab4ffab
nitaymayo <nitaymayo@gmail.com>
2024-12-08 19:39:17 +0200

    add Herd and Horse classes

---
+++ Herd.h
@@ -0,0 +1,20 @@
+//
+// Created by nitaymayo on 12/8/24.
+//
+
+#ifndef HERD_H
+#define HERD_H
+#include <memory>
+#include <Horse.h>
+
+class Herd {
+    int ID;
+    std::shared_ptr<Horse> leader_horses;
+
+
+
+};
+
+
+
+#endif //HERD_H
---
+++ Horse.h
@@ -0,0 +1,34 @@
+//
+// Created by nitaymayo on 12/8/24.
+//
+
+#ifndef HORSE_H
+#define HORSE_H
+#include <memory>
+
+
+class Horse {
+private:
+    int ID;
+    int speed;
+    std::shared_ptr<Horse> followGroup_next;
+    std::shared_ptr<Horse> followGroup_previous;
+    std::shared_ptr<Horse> first_follower;
+    std::shared_ptr<Herd> herd;
+
+public:
+    Horse(int ID, int speed): ID(ID), speed(speed) {};
+
+    ~Horse() = default;
+
+    int getID() const {
+        return ID;
+    }
+    int getSpeed() const {
+        return speed;
+    }
+};
+
+
+
+#endif //HORSE_H
