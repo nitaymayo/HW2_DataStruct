129cae42bc9847f9bea76cc26e761672dc8783d5
nitaymayo <nitaymayo@gmail.com>
2024-12-10 19:12:55 +0200

    implement last AVL fucntion and bug fix

--- AVLTree.h
+++ AVLTree.h
@@ -19,13 +19,14 @@ public:
 
   explicit AVLNode(T k):value(k),left(nullptr),right(nullptr),height(1){}
 
-  virtual ~AVLNode() {
-    delete left.get();
-    delete right.get();
-  }
-
   T data() {
     return value;
+  }
+  void setData(T data) {
+    this->value = data;
+  }
+  int getHeight() {
+    return height;
   }
 };
 
@@ -47,7 +48,7 @@ private:
   int balanceFactor(shared_ptr<AVLNode<T>> node);
 
   //Zagoury
-  AVLNode<T>* minValueNode(AVLNode<T>* node);
+  shared_ptr<AVLNode<T>> minValueNode(shared_ptr<AVLNode<T>> node);
 
   //Nitay V
   shared_ptr<AVLNode<T>> insert(shared_ptr<AVLNode<T>> node, T data);
@@ -55,6 +56,7 @@ private:
   //Nitay V
   shared_ptr<AVLNode<T>> deleteNode(shared_ptr<AVLNode<T>> node, T data);
 
+  shared_ptr<AVLNode<T>> search(shared_ptr<AVLNode<T>> node, T data);
 
 
   void printInorder(shared_ptr<AVLNode<T>> root)
@@ -70,25 +72,31 @@ public:
   AVLTree():root(nullptr){};
 
   //Nitay V
-  virtual ~AVLTree() {
-    delete root.get();
-  };
-
-  //Nitay V
   bool insert(T data);
 
   //Zagoury
-  T search(T data);
+  shared_ptr<AVLNode<T>> search(T data);
 
   //Nitay V
   bool deleteNode(T data);
 
-  void printInorder() {
+  void printInorder(){
     if (root != nullptr) {
       printInorder(root->left);
       cout << root->data() << " ";
       printInorder(root->right);
     }
+    cout << endl;
+  }
+
+  void printBalanceFactorsInorder(){printBalanceFactorsInorder(this->root); cout<<endl;}
+  void printBalanceFactorsInorder(shared_ptr<AVLNode<T>> node){
+    if (node != nullptr) {
+      printBalanceFactorsInorder(node->left);
+      cout << this->balanceFactor(node) << " ";
+      printBalanceFactorsInorder(node->right);
+    }
+
   }
 };
 
@@ -100,6 +108,41 @@ int AVLTree<T>::balanceFactor(shared_ptr<AVLNode<T>> node)
   return height(node->left) - height(node->right);
 }
 
+
+template<class T>
+shared_ptr<AVLNode<T>> AVLTree<T>::minValueNode(shared_ptr<AVLNode<T>> node) {
+  // if there isnt smaller node return this node
+  if (node->left == nullptr) return node;
+  // else go down and left in the tree
+  return minValueNode(node->left);
+}
+
+template<class T>
+shared_ptr<AVLNode<T>> AVLTree<T>::search(T data) {
+  if (this->root == nullptr) return nullptr;
+
+  try {
+    if (this->root->data() == data) return this->root;
+
+    if (this->root->data() > data) return search(this->root->left, data);
+
+    return search(this->root->right, data);
+  } catch (logic_error &e) {
+    return nullptr;
+  }
+}
+
+template<class T>
+shared_ptr<AVLNode<T>> AVLTree<T>::search(shared_ptr<AVLNode<T>> node, T data) {
+  if (node == nullptr) throw logic_error("Data not found");
+
+  if (node->data() == data) return node;
+
+  if (node->data() > data) return search(node->left, data);
+
+  return search(node->right, data);
+}
+
 template<class T>
 bool AVLTree<T>::insert(T data){
   if (this->root == nullptr){
@@ -108,9 +151,12 @@ bool AVLTree<T>::insert(T data){
   }
   try {
     if (this->root->data() > data){
-      this->insert(this->root->left, data);
+      if (this->root->left == nullptr) this->root->left = make_shared<AVLNode<T>>(data);
+      else this->insert(this->root->left, data);
+
     } else if (this->root->data() < data){
-      this->insert(this->root->right, data);
+      if (this->root->right == nullptr) this->root->right = make_shared<AVLNode<T>>(data);
+      else this->insert(this->root->right, data);
     } else {
       return false;
     }
@@ -118,7 +164,6 @@ bool AVLTree<T>::insert(T data){
     return false;
   }
   return true;
-
 }
 
 template<class T>
@@ -164,43 +209,54 @@ shared_ptr<AVLNode<T>> AVLTree<T>::insert(shared_ptr<AVLNode<T>> node, T data){
 
 template<class T>
 bool AVLTree<T>::deleteNode(T data) {
-  if (this->root == nullptr) return false;
+  try {
 
-  if (this->root->data() == data) {
-    this->root = nullptr;
+    if (this->root == nullptr) throw logic_error("Data not found");
+
+    if (this->root->data() == data) {
+      this->root = deleteNode(this->root, data);
+    }
+
+    if (this->root->data() > data) {
+      this->root->left = deleteNode(this->root->left, data);
+    } else {
+      this->root->right = deleteNode(this->root->right, data);
+    }
     return true;
+  } catch (logic_error &e) {
+    return false;
   }
-
-  if (this->root->data() > data) return deleteNode(this->root->left(), data);
-
-  return deleteNode(this->root->right(), data);
 }
 
 template<class T>
 shared_ptr<AVLNode<T>> AVLTree<T>::deleteNode(shared_ptr<AVLNode<T>> node, T data) {
-  if (node == nullptr) return false;
+
+  // in case the node to delete doesnt exist in the tree return false
+  if (node == nullptr) return nullptr;
+
 
   if (node->data() > data) {
     node->left = deleteNode(node->left, data);
   } else if (node->data() < data) {
     node->right = deleteNode(node->right, data);
-  } else {
-    if (node->left == nullptr || node->right == nullptr) {
-      shared_ptr<AVLNode<T>> subTree = node->left== nullptr ? node->right: node->left;
-      //if it has no child
-      if (subTree == nullptr) {
-        subTree = node;
-        node = nullptr;
-      } else {
-        // if it has 1 child
-        node = subTree;
-      }
+  } else if (node->data() == data) {
+    // this node should be deleted
+    // case if node has no son
+    if (node->left == nullptr && node->right == nullptr) {
+      return nullptr;
+    } else if (node->left == nullptr) {
+      // node has only right son
+      return node->right;
+    } else if (node->right == nullptr) {
+      // node has only left son
+      return node->left;
     } else {
       // Node has 2 children
       // switch between the smallest right son and node
       shared_ptr<AVLNode<T>> smallest_right_successor = minValueNode(node->right);
-      node->data = smallest_right_successor->data;
-      node->right = deleteNode(node->right, smallest_right_successor->data);
+      node->setData(smallest_right_successor->data());
+      // delete the smallest son (he is transferred to node location)
+      node->right = deleteNode(node->right, smallest_right_successor->data());
     }
 
     if (node == nullptr) return nullptr;
@@ -209,7 +265,7 @@ shared_ptr<AVLNode<T>> AVLTree<T>::deleteNode(shared_ptr<AVLNode<T>> node, T dat
     node->height = 1 + max(height(node->left), height(node->right));
     int balance = balanceFactor(node);
 
-    // Rotate if needed:
+    // check if rotation is needed - rotate and return new son
 
     // LL
     if (balance > 1 && balanceFactor(node->left) >= 0) return rotateRight(node);
@@ -232,23 +288,46 @@ shared_ptr<AVLNode<T>> AVLTree<T>::deleteNode(shared_ptr<AVLNode<T>> node, T dat
   return node;
 }
 
+template<class T>
+shared_ptr<AVLNode<T>> AVLTree<T>::rotateLeft(shared_ptr<AVLNode<T>> node) {
+  // save temp nodes
+  shared_ptr<AVLNode<T>> LR_sub_tree = node->right->left;
+  shared_ptr<AVLNode<T>> R_node = node->right;
+
+  // rotate
+  R_node->right = node;
+  node->left = LR_sub_tree;
+
+  // update height
+  node->height = 1 + max(height(node->left), height(node->right));
+  R_node->height = 1 + max(height(node->left), height(node->right));
+
+  //return new subtree root
+  return R_node;
+}
 
 template<class T>
 shared_ptr<AVLNode<T>> AVLTree<T>::rotateRight(shared_ptr<AVLNode<T>> node) {
-  //make rotation
-  shared_ptr<AVLNode<T>> LRsubTree = node->right->left;
-  shared_ptr<AVLNode<T>> left_node = node->right;
+  // save relevant nodes
+  shared_ptr<AVLNode<T>> LR_sub_tyree = node->right->left;
+  shared_ptr<AVLNode<T>> L_node = node->right;
 
-  node->left = LRsubTree;
-  left_node->right = node;
+  // rotate
+  node->left = LR_sub_tyree;
+  L_node->right = node;
 
   //update heights
   node->height = 1 + max(height(node->left), height(node->right));
-  left_node->height = 1 + max(height(node->left), height(node->right));
+  L_node->height = 1 + max(height(node->left), height(node->right));
 
-  return left_node;
+  // return new root
+  return L_node;
 }
 
+template<class T>
+int AVLTree<T>::height(shared_ptr<AVLNode<T>> node) {
+  return node ? node->getHeight() : 0;
+}
 
 
 #endif //AVLTREE_H
